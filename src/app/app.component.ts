f398970a964bcc6d599558da89ea434250de5c55
Morten Sjøgren <m_abs@mabs.dk>
2019-07-26 00:33:20 +0200

    it is a live

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as utils from 'tns-core-modules/utils/utils';
+
+// Reading https://www.raywenderlich.com/5573-media-playback-on-android-with-exoplayer-getting-started
+
+const exoplayer2 = com.google.android.exoplayer2;
 
 @Component({
   selector: 'ns-app',
   moduleId: module.id,
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  private get context() {
+    return utils.ad.getApplicationContext();
+  }
+  private trackSelector = new exoplayer2.trackselection.DefaultTrackSelector();
+  private loadControl = new exoplayer2.DefaultLoadControl();
+  private renderersFactory = new exoplayer2.DefaultRenderersFactory(this.context);
+
+  private exoPlayer = exoplayer2.ExoPlayerFactory.newSimpleInstance(this.context, this.renderersFactory, this.trackSelector, this.loadControl);
+  private playerListener = new exoplayer2.Player.EventListener({
+    onTimelineChanged(param0: com.google.android.exoplayer2.Timeline, param1: any, param2: number) {
+      console.log('onTimelineChanged', ...Array.from(arguments));
+    },
+    onTracksChanged(param0: com.google.android.exoplayer2.source.TrackGroupArray, param1: com.google.android.exoplayer2.trackselection.TrackSelectionArray) {
+      console.log('onTracksChanged', ...Array.from(arguments));
+    },
+    onLoadingChanged(param0: boolean) {
+      console.log('onLoadingChanged', ...Array.from(arguments));
+    },
+    onPlayerStateChanged(playWhenReady: boolean, playbackState: number) {
+      console.log('onPlayerStateChanged', ...Array.from(arguments));
+    },
+    onRepeatModeChanged(param0: number) {
+      console.log('onRepeatModeChanged', ...Array.from(arguments));
+    },
+    onShuffleModeEnabledChanged(param0: boolean) {
+      console.log('onShuffleModeEnabledChanged', ...Array.from(arguments));
+    },
+    onPlayerError(param0: com.google.android.exoplayer2.ExoPlaybackException) {
+      console.log('onPlayerError', ...Array.from(arguments));
+    },
+    onPositionDiscontinuity(param0: number) {
+      console.log('onPositionDiscontinuity', ...Array.from(arguments));
+    },
+    onPlaybackParametersChanged(param0: com.google.android.exoplayer2.PlaybackParameters) {
+      console.log('onPlaybackParametersChanged', ...Array.from(arguments));
+    },
+    onSeekProcessed() {
+      console.log('onSeekProcessed', ...Array.from(arguments));
+    },
+  });
+
+  public ngOnInit(): void {
+    this.exoPlayer.addListener(this.playerListener);
+
+    const userAgent = exoplayer2.util.Util.getUserAgent(this.context, 'mabs.dk');
+    const mediaSource = new exoplayer2.source.ExtractorMediaSource.Factory(new exoplayer2.upstream.DefaultDataSourceFactory(this.context, userAgent))
+      .setExtractorsFactory(new exoplayer2.extractor.DefaultExtractorsFactory())
+      .createMediaSource(android.net.Uri.parse('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'));
+
+    this.exoPlayer.prepare(mediaSource);
+
+    this.exoPlayer.setPlayWhenReady(true);
+  }
+
+  ngOnDestroy(): void {
+    this.exoPlayer.stop();
+    this.exoPlayer.removeListener(this.playerListener);
+    this.exoPlayer = null;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import * as nsApp from 'tns-core-modules/application';
+import { isAndroid } from 'tns-core-modules/ui/page/page';
+import * as utils from 'tns-core-modules/utils/utils';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemDetailComponent } from './item/item-detail.component';
@@ -10,6 +13,50 @@ import { ItemsComponent } from './item/items.component';
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+declare namespace dk {
+  namespace mabs {
+    class BackgroundService extends android.app.Service {}
+  }
+}
+
+if (isAndroid) {
+  @JavaProxy('dk.mabs.BackgroundService')
+  class BackgroundService extends android.app.Service {
+    onStartCommand(intent, flags, startId) {
+      console.log('start command', intent, flags, startId);
+      super.onStartCommand(intent, flags, startId);
+      return android.app.Service.START_STICKY;
+    }
+    onCreate() {
+      console.log('onCreate');
+      // Do something
+    }
+    onBind(param) {
+      // haven't had to deal with this, so i can't recall what it does
+      console.log('on Bind Services');
+
+      return super.onBind(param);
+    }
+    onUnbind(param) {
+      // haven't had to deal with this, so i can't recall what it does
+      console.log('UnBind Service');
+
+      return super.onUnbind(param);
+    }
+    onDestroy() {
+      console.log('service onDestroy');
+      // end service, reset any variables... etc...
+    }
+  }
+
+  nsApp.on(nsApp.displayedEvent, () => {
+    const context = utils.ad.getApplicationContext();
+    const intent = new android.content.Intent(context, dk.mabs.BackgroundService.class);
+    console.log('startService');
+    context.startService(intent);
+  });
+}
 
 @NgModule({
   bootstrap: [AppComponent],
